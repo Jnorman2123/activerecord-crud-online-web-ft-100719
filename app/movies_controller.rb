d67124f85959c5b32b80a5e125932c1f4a42ecc2
Jonathan Norman <jnorman2123@gmail.com>
2019-11-12 18:32:50 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -35,7 +35,7 @@ def can_be_created_in_a_block(args = { :title  => "Home Alone", :release_date =>
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.order(:id).first
+  # Movie.order(:id).first
   Movies.find(1)
 end
 
