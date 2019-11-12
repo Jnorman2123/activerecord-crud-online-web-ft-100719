c60705663dd6963eb56132230491c7e5f581c263
Jonathan Norman <jnorman2123@gmail.com>
2019-11-12 17:58:28 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,7 +6,7 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = Movie.find_or_create_by!(title: "This is a title")
+  movie = Movie.find_or_create_by(title: "This is a title")
   movie.title = "This is a title."
   __
 end
